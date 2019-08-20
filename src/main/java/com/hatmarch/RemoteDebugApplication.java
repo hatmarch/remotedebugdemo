c1e512c8f30d0390964033a93d2238a997b61d48
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2019-08-20 17:19:23 +1000

    Initial Commit

---
+++ src/main/java/com/hatmarch/RemoteDebugApplication.java
@@ -0,0 +1,12 @@
+package com.hatmarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteDebugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteDebugApplication.class, args);
+	}
+}
